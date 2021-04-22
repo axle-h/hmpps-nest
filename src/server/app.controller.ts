d2edb6e5afb86f4dedb39d3952d0b31cbfcf80d0
Alex Haslehurst <alex.haslehurst@gmail.com>
2021-04-22 09:12:36 +0100

    get-post-redirect validation

--- src/server/app.controller.ts
+++ src/server/app.controller.ts
@@ -1,10 +1,29 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ViewModel, ValidateAndRender } from './validation';
+
+class ExampleDto {
+  @IsString()
+  @IsNotEmpty()
+  stringValue: string;
+
+  @IsInt()
+  @Type(() => Number)
+  intValue: number;
+}
 
 @Controller()
 export class AppController {
   @Get()
   @Render('pages/index')
-  root() {
-    return {};
+  get(): ViewModel<ExampleDto> {
+    return { model: new ExampleDto() };
+  }
+
+  @Post()
+  @ValidateAndRender('pages/index')
+  post(@Body() model: ExampleDto): ViewModel<ExampleDto> {
+    return { model };
   }
 }
--- src/server/error.filter.ts
+++ src/server/error.filter.ts
@@ -1,6 +1,10 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 import { HealthError } from './health/types';
+import { ValidationException } from './validation';
 
+/**
+ * TODO break this up and move into respective modules.
+ */
 @Catch()
 export class ErrorFilter<T extends Error> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
@@ -10,14 +14,16 @@ export class ErrorFilter<T extends Error> implements ExceptionFilter {
       return response.status(HttpStatus.SERVICE_UNAVAILABLE).json(exception.health);
     }
 
+    if (exception instanceof ValidationException) {
+      return response.status(HttpStatus.BAD_REQUEST).render(exception.template, exception.viewModel);
+    }
+
     const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
     switch (status) {
       case HttpStatus.FORBIDDEN:
         return response.redirect('/login');
-      case HttpStatus.UNAUTHORIZED:
-      case HttpStatus.NOT_FOUND:
-      case HttpStatus.INTERNAL_SERVER_ERROR:
+      default:
         return response.status(status).render('pages/error', {
           message: exception.message,
           status,
